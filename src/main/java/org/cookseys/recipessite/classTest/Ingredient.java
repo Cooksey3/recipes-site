9e4e7fc6dbe2162313098558dd970f3d0371c607
Chris <chris@cookseys.org>
2018-05-04 13:44:58 -0400

    Tested & added type, quantity and name to vegetable

---
+++ src/main/java/org/cookseys/recipessite/classTest/Ingredient.java
@@ -0,0 +1,27 @@
+package org.cookseys.recipessite.classTest;
+
+public class Ingredient {
+
+	private String ingredient;
+	private Double quantity;
+	private String type;
+
+	public Ingredient(String ingredient, String type, double quantity) {
+		this.ingredient = ingredient;
+		this.quantity = quantity;
+		this.type = type;
+	}
+
+	public String getIngredientName() {
+		return ingredient;
+	}
+
+	public Double getQuantity() {
+		return quantity;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+}
--- src/test/java/org/cookseys/recipessite/classTest/IngredientTest.java
+++ src/test/java/org/cookseys/recipessite/classTest/IngredientTest.java
@@ -1,5 +1,64 @@
 package org.cookseys.recipessite.classTest;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
 public class IngredientTest {
 
+	@Test
+	public void ingredientShouldHaveName() {
+		Ingredient underTest = new Ingredient("Onions", "", 0.0);
+
+		String ingredient = underTest.getIngredientName();
+
+		assertThat(ingredient, is("Onions"));
+	}
+
+	@Test
+	public void ingredientShouldHaveDifferentName() {
+		Ingredient underTest = new Ingredient("Blueberries", "", 0.0);
+
+		String ingredient = underTest.getIngredientName();
+
+		assertThat(ingredient, is("Blueberries"));
+	}
+
+	@Test
+	public void ingredientShouldHaveQuantity() {
+		Ingredient underTest = new Ingredient("Onions", "", 4.0);
+
+		Double quantity = underTest.getQuantity();
+
+		assertThat(quantity, is(4.0));
+	}
+
+	@Test
+	public void ingredientShouldHaveDifferentQuantity() {
+		Ingredient underTest = new Ingredient("Onions", "Vegetable", 5.0);
+
+		Double quantity = underTest.getQuantity();
+
+		assertThat(quantity, is(5.0));
+	}
+	
+	@Test
+	public void ingredientShouldHaveType() {
+		Ingredient underTest = new Ingredient("Onions", "Vegetable", 5.0);
+		
+		String type = underTest.getType();
+		
+		assertThat(type, is("Vegetable"));
+	}
+
+	@Test
+	public void ingredientShouldHaveDifferentType() {
+		Ingredient underTest = new Ingredient("Blueberries", "Fruit", 5.0);
+		
+		String type = underTest.getType();
+		
+		assertThat(type, is("Fruit"));
+	}
+	
 }
